2ff73fc149906b5a5a8a045d64ce8d13780fe6a0
SantiagoCarranza <you@example.com>
2022-04-08 08:32:45 -0300

    desafio en rama

--- EJERCICIOS/Ej 3 - PINTAMEEEEEE - Santiago Carranza.py
+++ EJERCICIOS/Ej 3 - PINTAMEEEEEE - Santiago Carranza.py
@@ -1,11 +1,29 @@
-char = input("Ingrese el caracter que quiera que sea usado para formar la figura: ")
-ancho = int(input("Ingresá el ancho de la figura: "))
-alto = int(input("Ingresá el alto de la figura: "))
-
-
 def pintar():
     for i in range(0, alto):
         print(char * ancho)
 
 
-pintar()
+def pintar_triangulo(alto, char):
+    for i in range(alto):
+        print(" " * (alto - i - 1) + (char + " ") * (i + 1))
+
+
+opcion = int(
+    input(
+        "Si quiere dibujar un cuadrado ingrese 1, si quiere dibujar un triangulo ingrese 2: "
+    )
+)
+
+if opcion == 1:
+    char = input("Ingrese el caracter que quiera que sea usado para formar la figura: ")
+    ancho = int(input("Ingresá el ancho de la figura: "))
+    alto = int(input("Ingresá el alto de la figura: "))
+    pintar()
+
+elif opcion == 2:
+    char = input("Ingrese el caracter que quiera que sea usado para formar la figura: ")
+    alto = int(input("Ingresá el alto de la figura: "))
+    pintar_triangulo(alto, char)
+
+else:
+    print("Ingrese una opcion valida")
