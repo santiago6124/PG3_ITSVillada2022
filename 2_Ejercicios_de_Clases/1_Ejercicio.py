2687d7d7dc9b0f90032011329e6383bd1021efb7
Santiago Carranza <santiagocarranzazinny@gmail.com>
2022-04-22 10:28:12 -0300

    resolucion ej1

--- 2_Ejercicios_de_Clases/1_Ejercicio.py
+++ 2_Ejercicios_de_Clases/1_Ejercicio.py
@@ -1,18 +1,17 @@
 class Persona:
-
-    def inicializar(self,nombre):
-        self.nombre=nombre
+    def inicializar(self, nombre):
+        self.nombre = nombre
 
     def print(self):
-        print("Nombre",self.nombre)
+        print("Nombre", self.nombre)
 
 
-persona1=Persona()
-nombre=input("Ingrese el nombre de la persona 1 ")
+persona1 = Persona()
+nombre = input("Ingrese el nombre de la persona 1 ")
 persona1.inicializar(nombre)
 persona1.print()
 
-persona2=Persona()
-nombre=input("Ingrese el nombre de la persona 2 ")
+persona2 = Persona()
+nombre = input("Ingrese el nombre de la persona 2 ")
 persona2.inicializar(nombre)
 persona2.print()
