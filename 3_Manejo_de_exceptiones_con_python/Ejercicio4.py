39ea11f5c549c0d7e1fb332a09d494c18c2165c0
Santiago Carranza <santiagocarranzazinny@gmail.com>
2022-04-29 11:55:11 -0300

    resolucion ej4

---
+++ 3_Manejo_de_exceptiones_con_python/Ejercicio4.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        num1=int(input("Ingrese numerador: "))
+        num2=int(input("Ingrese denominador: "))
+        div=num1/num2
+        print("El resultado de la división es", div)
+        ans=input("Si no desea seguir dividiendo ingrese [n], si quiere seguir ingrese cualquier otra letra: ")
+        if ans=="n":
+            break  
+    except ZeroDivisionError:
+        print("Es imposible dividir por 0.")
+    except ValueError:
+        print("Usted NO ingresó un número, por favor ingrese correctamente los números")
